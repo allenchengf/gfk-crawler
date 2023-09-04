bb21171e2c35f8261f2b26466ffc59545f672a9a
Allen Chen <allencc77e@gmail.com>
2023-09-04 11:28:52 +0800

    check correct path by path

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ import os, stat
 import requests
 import time
 import logging
+import platform
 
 
 def main():
@@ -44,7 +45,8 @@ def main():
 
     try:
         logging_message("  Into Retailer Overview Page")
-        WebDriverWait(driver, 30, 0.5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="onetrust-close-btn-container"]/button')))
+        WebDriverWait(driver, 30, 0.5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="onetrust-close-btn'
+                                                                                       '-container"]/button')))
         # close cookie consent
         cookie_alert = driver.find_element(By.XPATH, '//*[@id="onetrust-close-btn-container"]/button')
         driver.execute_script("arguments[0].click();", cookie_alert)
@@ -77,11 +79,15 @@ def main():
         driver.get(links[i])
 
         try:
-            WebDriverWait(driver, 30, 0.5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="newron-content"]/div[1]/div[1]/div[8]/div/div[1]')))
-            click_download = driver.find_element(By.XPATH, '//*[@id="newron-content"]/div[1]/div[1]/div[7]/div/header/span[2]/div/span[1]/button')
+            WebDriverWait(driver, 30, 0.5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="newron-content'
+                                                                                           '"]/div[1]/div[1]/div['
+                                                                                           '8]/div/div[1]')))
+            click_download = driver.find_element(By.XPATH, '//*[@id="newron-content"]/div[1]/div[1]/div['
+                                                           '7]/div/header/span[2]/div/span[1]/button')
             driver.execute_script("arguments[0].click();", click_download)
 
-            download = driver.find_element(By.XPATH, '//*[@id="newron-content"]/div[1]/div[1]/div[7]/div/header/span[2]/div[2]/div[1]/div/footer/button[1]')
+            download = driver.find_element(By.XPATH, '//*[@id="newron-content"]/div[1]/div[1]/div[7]/div/header/span['
+                                                     '2]/div[2]/div[1]/div/footer/button[1]')
             driver.execute_script("arguments[0].click();", download)
             
         except requests.exceptions.RequestException as e:
@@ -97,17 +103,29 @@ def main():
 
 
 def download_file_rename(name):
+    correct_path = get_path_by_os()
     lists = os.listdir(os.environ['DOWNLOAD_PATH'])
-    lists.sort(key=lambda fn:os.path.getmtime(os.environ['DOWNLOAD_PATH'] + "\\" + fn))
+    lists.sort(key=lambda fn: os.path.getmtime(os.environ['DOWNLOAD_PATH'] + correct_path + fn))
     latest_file = lists.pop()
-    os.chmod(os.environ['DOWNLOAD_PATH'] +"\\" + latest_file, stat.S_IRWXU)
-    os.rename(os.environ['DOWNLOAD_PATH'] +"\\" + latest_file, os.environ['DOWNLOAD_PATH'] + "\\" + name + "_" + time.strftime('%Y%m%d_%H_%M_%S') + '.xlsx')
-    logging_message("  Download " + os.environ['DOWNLOAD_PATH'] + "\\" + name + "_" + time.strftime('%Y%m%d_%H_%M_%S') + '.xlsx')
+    os.chmod(os.environ['DOWNLOAD_PATH'] + correct_path + latest_file, stat.S_IRWXU)
+    os.rename(os.environ['DOWNLOAD_PATH'] + correct_path + latest_file,
+              os.environ['DOWNLOAD_PATH'] + correct_path + name + "_" + time.strftime('%Y%m%d_%H_%M_%S') + '.xlsx')
+    logging_message("  Download " + os.environ['DOWNLOAD_PATH'] + correct_path
+                    + name + "_" + time.strftime('%Y%m%d_%H_%M_%S') + '.xlsx')
+
+
+def get_path_by_os():
+    os_name = platform.system()
+    if os_name == 'Windows':
+        return '\\'
+    else:
+        return '/'
 
 
 def logging_message(message):
     print(message)
-    logging.basicConfig(level=logging.INFO, filename='accesslog ' + time.strftime('%Y%m%d_%H_%M_%S') + '.log', filemode='a',format='%(asctime)s %(levelname)s: %(message)s')
+    logging.basicConfig(level=logging.INFO, filename='accesslog ' + time.strftime('%Y%m%d_%H_%M_%S') + '.log',
+                        filemode='a',format='%(asctime)s %(levelname)s: %(message)s')
     logging.info(message)
 
 
